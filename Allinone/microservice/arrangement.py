147a2b89b348f782da68ccb7a9eae34a4316a13d
Yep Zhi Xuan <zhixuanyep15@gmail.com>
2024-10-09 19:22:54 +0800

    bug fixes
    added remarks column for managers, changed some urls to make them more coherent

--- Allinone/microservice/arrangement.py
+++ Allinone/microservice/arrangement.py
@@ -10,7 +10,7 @@ from os import environ
 from flask_cors import CORS
 import os
 import sys
-from datetime import date
+from datetime import date, timedelta
 
 import json
 
@@ -49,27 +49,32 @@ class Request(db.Model):
     timeslot = db.Column(db.Integer, nullable=False)  # Morning - 1, Afternoon - 2, Full Day - 3
     status = db.Column(db.String(20), nullable=False, default='Pending')  # Pending, Approved, Rejected
     reason = db.Column(db.String(255), nullable=False, default="") # Reason for WFH request
+    remarks = db.Column(db.String(255), nullable=True)
 
     employee = relationship("Employee", backref="requests")
 
-    def __init__(self, staff_id, manager_id, request_date, arrangement_date, timeslot, reason, status='Pending'):
+    def __init__(self, request_id, staff_id, manager_id, request_date, arrangement_date, timeslot, reason, remarks, status='Pending'):
+        self.request_id = request_id
         self.staff_id = staff_id
         self.manager_id = manager_id
-        self.arrangement_date = arrangement_date
         self.request_date = request_date
+        self.arrangement_date = arrangement_date
         self.timeslot = timeslot
         self.reason = reason
         self.status = status
+        self.remarks = remarks
 
     def json(self):
         return {
+            'request_id': self.request_id,
             'staff_id': self.staff_id,
             'manager_id': self.manager_id,
             'arrangement_date': str(self.arrangement_date),
             'request_date': str(self.request_date),
             'timeslot': self.timeslot,
             'reason': self.reason,
-            'status': self.status
+            'status': self.status,
+            'remarks': self.remarks
         }
     
 # Create a new WFH request
@@ -77,13 +82,29 @@ class Request(db.Model):
 def create_request():
     try:
         data = request.json
+
+        # check for valid date range
+        arrangement_date = data.get("arrangement_date")
+        arrangement_date = date.fromisoformat(arrangement_date)
+        request_date = data.get("request_date")
+        request_date = date.fromisoformat(request_date)
+
+        earliest_valid_date = request_date - timedelta(days=60) # 2 months prior
+        latest_valid_date = request_date + timedelta(days=90) # 3 months in advance
+
+        if arrangement_date < earliest_valid_date or arrangement_date > latest_valid_date:
+            return jsonify({"message": "Requested day is outside the valid range. Date must be between 2 months prior and 3 months in advance.",
+                            "code": 400}), 400
+
         new_request = Request(
-            staff_id=data["staff_id"],
-            manager_id=data["manager_id"],
-            arrangement_date=data["arrangement_date"],
-            request_date= data["request_date"],
-            timeslot = data["timeslot"],
-            reason = data["reason"],
+            request_id=data.get("request_id"),
+            staff_id=data.get("staff_id"),
+            manager_id=data.get("manager_id"),
+            request_date = data.get("request_date"),
+            arrangement_date=data.get("arrangement_date"),
+            timeslot = data.get("timeslot"),
+            reason = data.get("reason"),
+            remarks = data.get("remarks")
         )
         # print(new_request)
         db.session.add(new_request)
@@ -105,7 +126,7 @@ def get_all_requests():
         return jsonify({'message': 'Failed to retrieve requests', 'code': 500}), 500
     
 # Retrieve a specific WFH request
-@app.route('/request/<int:request_id>', methods=['GET'])
+@app.route('/get_request/<int:request_id>', methods=['GET'])
 def get_request(request_id):
     try:
         request = Request.query.filter_by(request_id=request_id).first()
@@ -118,7 +139,7 @@ def get_request(request_id):
         return jsonify({'message': 'Failed to retrieve request', 'code': 500}), 500
 
 #Retrieve a WFH request by staff
-@app.route('/requests/staff/<int:staff_id>', methods=['GET'])
+@app.route('/get_requests/staff/<int:staff_id>', methods=['GET'])
 def get_requests_by_staff_id(staff_id):
     try:
         requests = Request.query.filter_by(staff_id=staff_id).all()
@@ -131,13 +152,14 @@ def get_requests_by_staff_id(staff_id):
         return jsonify({'message': 'Failed to retrieve requests by staff ID', 'code': 500}), 500
     
 # Update request status
-@app.route('/request/<int:request_id>', methods=['PUT'])
+@app.route('/update_request/<int:request_id>', methods=['PUT'])
 def update_request(request_id):
     try:
         data = request.json
         request_to_update = Request.query.filter_by(request_id=request_id).first()
         if request_to_update:
             request_to_update.status = data.get('status', request_to_update.status)
+            request_to_update.remarks = data.get('remarks', "")
             db.session.commit()
             return jsonify({'message': 'Request updated', 'data': request_to_update.json(), 'code': 200}), 200
         else:
